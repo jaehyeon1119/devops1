703c8646c263d5b29008bbf91d7fbfbc7961d3e8
jaehyeon1119 <leajae@naver.com>
2026-04-09 09:49:51 +0900

    devops1 v1.0

---
+++ devops_product/src/main/java/org/cloud/dto/ProductDTO.java
@@ -0,0 +1,37 @@
+package org.cloud.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+
+	private int num;
+	private String name;
+	private int price;
+	private int amount;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+}
